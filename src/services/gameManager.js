4e887e8624358fe9d3c68a5034a2d0477b78bb86
pRin-ce13 <202301178@daiict.ac.in>
2025-12-14 23:28:19 +0530

    Added GameManager and renamed game controller

---
+++ src/services/gameManager.js
@@ -0,0 +1,41 @@
+import{
+    createEmptyBoard,
+    checkWinner,
+    applyMove,
+    isMoveValid
+} from "./gameService.js";
+
+import { findBestMove } from "../ai/minimax.js";
+
+export class GameManager {
+  constructor(mode = "ai") {
+    this.board = createEmptyBoard();
+    this.currentPlayer = "X";
+    this.winner = null;
+    this.mode = mode;
+  }
+
+  playHumanMove(index) {
+    if(this.winner || !isMoveValid(this.board, index)) return false;
+    this.board = applyMove(this.board, index, this.currentPlayer);
+    this.winner = checkWinner(this.board);
+    if(!this.winner) {
+
+        if(this.currentPlayer === "X") this.currentPlayer = "O";
+        else this.currentPlayer = "X";
+    }
+    return true;
+    }
+
+  playAIMove() {
+    // TODO
+    if(this.winner) return false;
+    const bestMove = findBestMove(this.board, "O","X");
+    this.board = applyMove(this.board, bestMove, "O");
+    this.winner = checkWinner(this.board);
+    if(!this.winner) {
+     this.currentPlayer = "X";
+    }
+    return true;
+  }
+}
